fb74690b147519bde3e651aad65f94da63c50807
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-04-13 13:05:42 -0700

    add letter opener when resetting password in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
