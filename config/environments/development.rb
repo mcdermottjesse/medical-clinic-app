fc3c2b8eb20d083ca8f887be908e02b46d886ef8
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-04-13 12:44:48 -0700

    set up password reset email link

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,16 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password:ENV["GMAIL_PASSWORD"]
+  }
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Raises error for missing translations.
