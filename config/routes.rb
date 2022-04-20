0ad0733ec7c00c322a9e63715b320a1d54118cd1
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-04-19 18:36:30 -0700

    add authorized unauthorized root paths

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,15 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+devise_scope :user do
+  authenticated :user do
+    root 'home#index', as: :authenticated_root
+  end
+end
+
+  unauthenticated do
+    root 'devise/sessions#new', as: :unauthenticated_root
+  end
 
   namespace :admin do
     resources :users
