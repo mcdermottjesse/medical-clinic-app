2fa14ece84e1f594fc89d93a4ce99659453af876
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-05-14 17:51:05 -0700

    center user invite form, add user validations and add select options for account type and location

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :rememberable, :validatable, :invitable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  ACCOUNTS = ["Admin", "Manager", "Nurse", "Doctor", "Care Worker"]
+  LOCATIONS = ["Victoria General", "Royal Jubilee", "Sanich Penisula", "Nanaimo Regional"]
 end
