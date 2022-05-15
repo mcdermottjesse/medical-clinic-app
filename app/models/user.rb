865e277f76897158c08a71bc30e4196657f56aea
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-05-15 16:33:51 -0700

    readd devise recoverable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :rememberable, :validatable, :invitable
+  devise :invitable, :database_authenticatable, :recoverable, :rememberable, :validatable
 
   validates :first_name, presence: true
   validates :last_name, presence: true
