0b147b3d5bfccd5dcb19fb2938bab6e69a335f1b
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-05-16 13:56:13 -0700

    start password customization

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
+  # validates_format_of :password, with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}]]$\z/
+
   ACCOUNTS = ["Admin", "Manager", "Nurse", "Doctor", "Care Worker"]
   LOCATIONS = ["Victoria General", "Royal Jubilee", "Sanich Penisula", "Nanaimo Regional"]
 end
