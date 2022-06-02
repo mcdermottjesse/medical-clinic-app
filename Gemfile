548e2574b96ae1a133a200088e79d661e3558c31
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-06-02 12:28:14 -0700

    install and start implementation of pundit

--- Gemfile
+++ Gemfile
@@ -40,6 +40,9 @@ gem "redis", "~> 4.0"
 
 gem 'devise'
 gem 'devise_invitable', '~> 2.0.0'
+
+gem "pundit"
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
+  include Pundit::Authorization
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:accept_invitation, keys: [:first_name, :last_name, :email, :password, :password_confirmation])
-    end
+  protected
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_back(fallback_location: admin_users_path)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:accept_invitation, keys: [:first_name, :last_name, :email, :password, :password_confirmation])
+  end
 end
