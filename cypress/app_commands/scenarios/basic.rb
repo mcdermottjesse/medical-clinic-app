07c2011189ddfeda8d1be73c5573ada0f653d2b9
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-06-07 20:31:22 -0700

    add edit create user and filter by location tests

--- cypress/app_commands/scenarios/basic.rb
+++ cypress/app_commands/scenarios/basic.rb
@@ -1,9 +1,17 @@
 User.create!(
-  id: 1,
   first_name: "Cypress",
   last_name: "Test",
   location: "Victoria General",
   email: "test@cypress.com",
+  password: "Testonly1!",
+  account_type: "Admin",
+)
+
+User.create!(
+  first_name: "Second",
+  last_name: "Test",
+  location: "Nanaimo Regional",
+  email: "second@cypress.com",
   password: "Testonly1!",
   account_type: "Admin",
 )
