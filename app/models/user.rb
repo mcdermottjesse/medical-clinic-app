f11258367fb3925a859c5a203cead024c824aa64
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-06-30 18:54:19 -0700

    readd deleted search method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,21 @@
 class User < ApplicationRecord
+  class << self
+    include Search
+  end
+
+  include Name
+
   attr_accessor :skip_password_validation
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :recoverable, :rememberable, :validatable
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :first_name, :last_name, :account_type, :location, presence: true
 
   validates :password, format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}\z/, message: :validation_message }, unless: :skip_password_validation
 
   ACCOUNTS = ["Admin", "Manager", "Nurse", "Doctor", "Care Worker"]
   LOCATIONS = ["All Locations", "Victoria General", "Royal Jubilee", "Sanich Penisula", "Nanaimo Regional"]
-
-  def full_name
-    [first_name, last_name].join(" ")
-  end
 
   def admin?
     account_type == "Admin"
