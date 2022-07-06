69d1aee91cfde09bbe4335d05964abd8706b0d3a
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-07-05 20:27:07 -0700

    show other pronoun input when other pronoun option is selected

--- app/javascript/helpers/hide.js
+++ app/javascript/helpers/hide.js
@@ -1,16 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const pronounSelect = document.getElementById('pronoun-select')
-  const otherPronounInput = document.getElementById('hide-pronoun')
-  otherPronounInput.style.display = 'none';
-  
+  const otherPronounInput = document.getElementById('hide-pronoun').style.display
+  otherPronounInput = 'none';
   document.addEventListener('change', (event) => {
-    if (event.target.matches('[name="client[pronoun]"]')) {
-      console.log(event);
-      console.log('You selected: ', this.value);
-      //figure out how to get option value = "Other"
-      otherPronounInput.style.display = 'flex';
-    } else {
-      otherPronounInput.style.display = 'none';
-    }
+    const selectOption = event.target.value
+    selectOption === "Other" ? otherPronounInput = 'block' : otherPronounInput = 'none';
   });
 })
