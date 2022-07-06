1ea04985220da5d3346c02579bd9955203377a65
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-07-05 21:51:57 -0700

    generate random string for client code before create

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,5 +5,12 @@ class Client < ApplicationRecord
 
   include Name, Location
 
+  before_create :generate_client_code
+
   PRONOUNS = ["She/Her", "They/Them", "He/Him", "Other"]
+
+  def generate_client_code
+    self.client_code = first_name[0] + last_name[0] + ([*('A'..'Z'), *('0'..'9')]).sample(4).join
+    # * = splat operator, it destructures an array
+  end
 end
