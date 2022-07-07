56f682e30f84cdefb891f2b99cde4568a75b1015
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-07-07 00:22:39 -0700

    regenerate client code if duplicate

--- app/models/client.rb
+++ app/models/client.rb
@@ -9,8 +9,17 @@ class Client < ApplicationRecord
 
   PRONOUNS = ["She/Her", "They/Them", "He/Him", "Other"]
 
+  
   def generate_client_code
-    self.client_code = first_name[0] + last_name[0] + ([*('A'..'Z'), *('0'..'9')]).sample(4).join
-    # * = splat operator, it destructures an array
+    initials = first_name[0] + last_name[0]
+    random_string = ([*('A'..'Z'), *('0'..'9')]).sample(4).join
+    # * = splat operator, it destructures array
+    self.client_code = initials + random_string
+    if Client.pluck(:client_code).detect { |code| code == self.client_code }
+      self.client_code = self.client_code.slice(0..1) + random_string
+      # ^ will only generate new client code again once. therefore will still be created even if updated client code is duplicate again
+      # explore how to keep regenerating client codes until they are unique
+      # maybe add in birthday info (refer to trailer)
+    end
   end
 end
