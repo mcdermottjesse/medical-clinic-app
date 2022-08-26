b89b81a344abd544f4b68639c2e65d40b8e5052f
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-08-25 17:44:12 -0700

    add checkbox to skip healthcard validations if no healthcard provided

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,8 +1,10 @@
 class ClientsController < ApplicationController
   before_action :set_client, only: [:edit, :update, :destroy]
+  before_action :skip_validation_health_card, :skip_validation_consent, only: [:update]
+  before_action :health_card_checkbox_checked, only: [:edit]
 
   def index
-    @location_param == 'All Locations' ? clients = Client.all.order('last_name ASC') : clients = Client.where(location: @location_param).order('last_name ASC')
+    @location_param == "All Locations" ? clients = Client.all.order("last_name ASC") : clients = Client.where(location: @location_param).order("last_name ASC")
     @clients = clients.paginate(page: params[:page], per_page: 6)
   end
 
@@ -15,12 +17,13 @@ class ClientsController < ApplicationController
 
   def create
     @client = Client.new(client_params)
+    skip_validation_health_card
     respond_to do |format|
       if @client.save
-        format.html { redirect_to clients_path(@client, location: @client.location), notice: 'Client successfully created' }
+        format.html { redirect_to clients_path(@client, location: @client.location), notice: "Client successfully created" }
         format.json { render :index, status: :created, location: @client }
       else
-        flash.now[:alert] = 'There was an error creating the Client'
+        flash.now[:alert] = "There was an error creating the Client"
         format.html { render :new }
         format.json { render json: @client.errors, status: :unprocessable_entity }
       end
@@ -31,13 +34,12 @@ class ClientsController < ApplicationController
   end
 
   def update
-    @client.skip_consent_validation = true
     respond_to do |format|
       if @client.update(client_params)
-        format.html { redirect_to clients_path(location: @client.location), notice: 'Client successfully updated' }
-        format.json { render :index, status: :ok, location: @client}
+        format.html { redirect_to clients_path(location: @client.location), notice: "Client successfully updated" }
+        format.json { render :index, status: :ok, location: @client }
       else
-        flash.now[:alert] = 'There was an error updating the Client'
+        flash.now[:alert] = "There was an error updating the Client"
         format.html { render :edit }
         format.json { render json: @client.errors, status: :unprocessable_entity }
       end
@@ -48,10 +50,10 @@ class ClientsController < ApplicationController
     @client.destroy
     respond_to do |format|
       if @client.destroyed?
-        format.html { redirect_to clients_path(location: @location_param), notice: 'Client successfully destroyed' }
+        format.html { redirect_to clients_path(location: @location_param), notice: "Client successfully destroyed" }
         format.json { head :no_content }
       else
-        flash.now[:alert] = 'There was an error deleting the Client'
+        flash.now[:alert] = "There was an error deleting the Client"
         format.html { render :edit }
         format.json { render json: @client.errors, status: :unprocessable_entity }
       end
@@ -83,5 +85,18 @@ class ClientsController < ApplicationController
 
   def set_client
     @client = Client.find(params[:id])
+  end
+
+  def skip_validation_health_card
+    @client.skip_health_card_validation = true if params["client"]["valid_health_card"].eql?("1")
+  end
+
+  def skip_validation_consent
+    @client.skip_consent_validation = true
+  end
+
+  def health_card_checkbox_checked
+    @health_card_number_present_check = @client.health_card_number.blank? && @client.health_card_number_validation.blank?
+    @health_card_expiry_present_check = @client.health_card_expiry.blank? && @client.health_card_expiry_validation.blank?
   end
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,8 +1,7 @@
 class Client < ApplicationRecord
+  attr_accessor :skip_consent_validation, :skip_health_card_validation
 
-  attr_accessor :skip_consent_validation
-
-  validates :first_name, :last_name, :dob, :location, :pronoun, :health_card_expiry, :bed_number, :emergency_contact_name, presence: true
+  validates :first_name, :last_name, :dob, :location, :pronoun, :bed_number, :emergency_contact_name, presence: true
 
   validates :consent, presence: true, unless: :skip_consent_validation
 
@@ -10,14 +9,18 @@ class Client < ApplicationRecord
 
   validate :dob_validation, :health_card_expiry_validation # validate to render past/future date error message
 
-  before_validation :format_health_number, :format_phone_number, :format_emergency_phone_number
+  before_validation :format_phone_number, :format_emergency_phone_number
 
-  validates_format_of :health_card_number, with: /\A[0-9]{4}-[0-9]{3}-[0-9]{3}\z/ # format = 1234-567-890
+  validates :health_card_expiry, presence: true, unless: :skip_health_card_validation
+
+  before_validation :format_health_number, unless: :skip_health_card_validation
+
+  validates_format_of :health_card_number, with: /\A[0-9]{4}-[0-9]{3}-[0-9]{3}\z/, unless: :skip_health_card_validation # format = 1234-567-890
 
   validates_format_of :phone_number, :emergency_contact_info, with: /\A\(([0-9]{3}+)\) [0-9]{3}-[0-9]{4}\z/ # format = (123) 456-7890
 
   before_save :capitalize_name
-  
+
   before_create :generate_client_code
 
   before_update :clear_other_pronoun
@@ -28,41 +31,41 @@ class Client < ApplicationRecord
 
   include Name, Location, ValidationMessage
 
-  PRONOUNS = ['She/Her', 'They/Them', 'He/Him', 'Other']
+  PRONOUNS = ["She/Her", "They/Them", "He/Him", "Other"]
 
   private
 
   def format_health_number
     correct_format = /\A[0-9]{4}-[0-9]{3}-[0-9]{3}\z/
-    health_num_array = health_card_number.split('')
+    health_num_array = health_card_number.split("")
 
     if health_card_number !~ correct_format && health_num_array.length >= 10 && health_num_array.length <= 12
-      self.health_card_number = self.health_card_number.gsub(/\W/, '').insert(4, '-') # gsub(/\W/, '') removes all symbols e.g '#$%^' etc
-      self.health_card_number = self.health_card_number.insert(8, '-')
+      self.health_card_number = self.health_card_number.gsub(/\W/, "").insert(4, "-") # gsub(/\W/, '') removes all symbols e.g '#$%^' etc
+      self.health_card_number = self.health_card_number.insert(8, "-")
     end
   end
 
   def format_phone_number
     correct_format = /\A\(([0-9]{3}+)\) [0-9]{3}-[0-9]{4}\z/
-    phone_num_array = phone_number.split('')
+    phone_num_array = phone_number.split("")
 
     if phone_number !~ correct_format && phone_num_array.length >= 10 && phone_num_array.length <= 14
-      self.phone_number = self.phone_number.gsub(/\W/, '').insert(0, '(')
-      self.phone_number = self.phone_number.insert(4, ')')
-      self.phone_number = self.phone_number.insert(5, ' ')
-      self.phone_number = self.phone_number.insert(9, '-')
+      self.phone_number = self.phone_number.gsub(/\W/, "").insert(0, "(")
+      self.phone_number = self.phone_number.insert(4, ")")
+      self.phone_number = self.phone_number.insert(5, " ")
+      self.phone_number = self.phone_number.insert(9, "-")
     end
   end
 
   def format_emergency_phone_number
     correct_format = /\A\(([0-9]{3}+)\) [0-9]{3}-[0-9]{4}\z/
-    emerg_phone_array = emergency_contact_info.split('')
+    emerg_phone_array = emergency_contact_info.split("")
 
     if emergency_contact_info !~ correct_format && emerg_phone_array.length >= 10 && emerg_phone_array.length <= 14
-      self.emergency_contact_info = self.emergency_contact_info.gsub(/\W/, '').insert(0, '(')
-      self.emergency_contact_info = self.emergency_contact_info.insert(4, ')')
-      self.emergency_contact_info = self.emergency_contact_info.insert(5, ' ')
-      self.emergency_contact_info = self.emergency_contact_info.insert(9, '-')
+      self.emergency_contact_info = self.emergency_contact_info.gsub(/\W/, "").insert(0, "(")
+      self.emergency_contact_info = self.emergency_contact_info.insert(4, ")")
+      self.emergency_contact_info = self.emergency_contact_info.insert(5, " ")
+      self.emergency_contact_info = self.emergency_contact_info.insert(9, "-")
     end
   end
 
@@ -78,14 +81,13 @@ class Client < ApplicationRecord
     counter = 0
     loop do
       counter += 1
-      
-      self.client_code = first_name[0].capitalize + last_name[0].capitalize + ([*('A'..'Z'), *('0'..'9')]).sample(4).join + dob.strftime('%y')
+
+      self.client_code = first_name[0].capitalize + last_name[0].capitalize + ([*("A".."Z"), *("0".."9")]).sample(4).join + dob.strftime("%y")
       # * = splat operator, it destructures array
-      
+
       raise ClientException.new("Could not find a unique Client Code. Please try again. If problem persists please contact Site Adminstrator") if counter > 100
 
       break unless Client.pluck(:client_code).include?(self.client_code)
-
     end
   end
 end
