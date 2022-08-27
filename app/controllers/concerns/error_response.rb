4bfa758f49a807f513c8acd8776c1ac2e05f1652
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-08-26 17:44:50 -0700

    start styling internal server 500 error page

--- app/controllers/concerns/error_response.rb
+++ app/controllers/concerns/error_response.rb
@@ -4,20 +4,21 @@ module ErrorResponse
   included do
     around_action :handle_errors
   end
-
+  # this handles 500 internal server errors
+  # may need to create a new controller for 404 page not found errors - reference https://mattbrictson.com/dynamic-rails-error-pages
   def handle_errors
     begin
       yield
     rescue ClientException => e
       @error = e
-      render partial: "shared/error"
+      render "errors/error"
     rescue UserException => e
       @error = e
-      render partial: "shared/error"
+      render "errors/error"
     rescue => e
       @development_error = e # display error ourput for development only! delete in production
       @error = "Something went wrong. Please try again. If problem persists please contact Site Adminstrator"
-      render partial: "shared/error"
+      render "errors/error"
     end
   end
 end
