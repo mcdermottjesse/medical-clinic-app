b0003f708d17f48441ff92107a5d4de01b3bece7
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-08-31 18:21:30 -0700

    fix text overflow

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -10,7 +10,7 @@ class ClientsController < ApplicationController
 
   def show
     @client.other_pronoun.present? ? @client_pronoun = @client.other_pronoun : @client_pronoun = @client.pronoun
-    @health_card_number_label = "Health Card Number: " if @client.health_card_number.present?
+    @health_card_number_label = "Health Number: " if @client.health_card_number.present?
     @health_card_expiry_label = "Health Card Expiry: " if @client.health_card_expiry.present?
     @email_label = "Email: " if @client.email.present?
     @general_info_label = "General Info: " if @client.general_info.present? 
