82bcd82bd352d1e004cbadef2dd262f4e0ec3d60
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-09-09 15:36:29 -0700

    close navbar dropdown on outerclick

--- app/javascript/helpers/NavbarDropdown.js
+++ app/javascript/helpers/NavbarDropdown.js
@@ -1,10 +1,12 @@
 const dropDownMenu = document.querySelector('.nav-dropdown');
 if (dropDownMenu) {
-	document.addEventListener('DOMContentLoaded', () => {
-    dropDownMenu.style.display = 'none';
-    const navButton = document.querySelector('.nav-btn')
-    navButton.onclick = () =>  {
+  dropDownMenu.style.display = 'none';
+  const navButton = document.querySelector('.nav-btn')
+  document.addEventListener('click', (e) =>  {
+    if (e.target.parentNode === navButton) {
       dropDownMenu.style.display === 'none' ? dropDownMenu.style.display = 'block' : dropDownMenu.style.display = 'none';
-     }
+    } else {
+      dropDownMenu.style.display = 'none';
+    }
   });
 }
