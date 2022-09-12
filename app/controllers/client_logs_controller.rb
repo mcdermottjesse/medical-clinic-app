79699dd51324f612f3f06a0511eb87c15e2a5a09
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-09-12 14:30:29 -0700

    add update action for client logs

--- app/controllers/client_logs_controller.rb
+++ app/controllers/client_logs_controller.rb
@@ -1,5 +1,7 @@
 class ClientLogsController < ApplicationController
-  before_action :set_client, only: [:index, :new, :create]
+  before_action :set_client
+  before_action :set_client_log, only: [:edit, :update]
+
   def index
     @client_log = ClientLog.where(client_id: @client)
   end
@@ -31,6 +33,17 @@ class ClientLogsController < ApplicationController
   end
 
   def update
+    @client_log.user_id = current_user.id
+    respond_to do |format|
+      if @client_log.update(client_log_params)
+        format.html { redirect_to client_client_logs_path(@client, location: @location_param), notice:  "Client Log for #{@client.full_name} successfully updated"}
+        format.json {render :index, status: :ok, location: @client_log }
+      else
+        flash.now[:alert] = "There was an error updating the Client Log"
+        format.html { render :edit }
+        format.json { render json: @client_log.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
@@ -48,5 +61,9 @@ class ClientLogsController < ApplicationController
 
   def set_client
     @client = Client.find(params[:client_id])
+  end
+
+  def set_client_log
+    @client_log = ClientLog.find(params[:id])
   end
 end
