2335d18f049255f4e785a8179aa3066f27a8f7da
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-10-04 12:02:21 -0700

    reformat medication javascript

--- app/javascript/helpers/Medication.js
+++ app/javascript/helpers/Medication.js
@@ -21,42 +21,29 @@ if (medForm) {
 			});
 		}
 
-		function showClass(elements) {
-			elements.forEach((elem) => {
-				document.querySelector(elem).style.display = 'block';
-			});
-		}
-
-		hideClass([ '.add-med-1', '.add-med-2', '.add-med-3' ]);
-
 		hideId([
 			'medication_medication_names_attributes_1_name',
 			'medication_medication_names_attributes_2_name',
 			'medication_medication_names_attributes_3_name',
 			'medication_medication_names_attributes_4_name'
 		]);
-  
-		document.addEventListener('click', (e) => {
-			medBtn = e.target.className;
-			console.log(medBtn);
-			if (medBtn === 'add-med-0') {
-				hideClass([ '.add-med-0', '.filter-med' ]);
+		counter = 0;
+		addMedd.addEventListener('click', () => {
+			counter++;
+
+			if (counter === 1) {
+				hideClass([ '.filter-med' ]);
 				hideId([ 'medication_query' ]);
 				showId([ 'medication_medication_names_attributes_1_name' ]);
-				showClass([ '.add-med-1' ]);
 			}
-			if (medBtn === 'add-med-1') {
-				hideClass([ '.add-med-1' ]);
+			if (counter === 2) {
 				showId([ 'medication_medication_names_attributes_2_name' ]);
-				showClass([ '.add-med-2' ]);
 			}
-			if (medBtn === 'add-med-2') {
-				hideClass([ '.add-med-2' ]);
+			if (counter === 3) {
 				showId([ 'medication_medication_names_attributes_3_name' ]);
-				showClass([ '.add-med-3' ]);
 			}
-			if (medBtn === 'add-med-3') {
-				hideClass([ '.add-med-3' ]);
+			if (counter === 4) {
+				hideClass([ '.add-med' ]);
 				showId([ 'medication_medication_names_attributes_4_name' ]);
 			}
 		});
