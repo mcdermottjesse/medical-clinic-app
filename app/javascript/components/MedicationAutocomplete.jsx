9a9a2e964a3e42db81eac46bd78af22c35596eb4
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-10-24 10:22:53 -0700

    implement onClickHandler and outerClickHandler for client medications

--- app/javascript/components/MedicationAutocomplete.jsx
+++ app/javascript/components/MedicationAutocomplete.jsx
@@ -1,20 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const inputRef = React.createRef();
+
 const MedicationAutocomplete = () => {
 	const [ medications, setMedications ] = useState([]);
 	const [ text, setText ] = useState('');
 	const [ suggestions, setSuggestions ] = useState([]);
 	const [ open, setOpen ] = useState(false);
 
+	const handleOuterClick = (e) => {
+		if (!inputRef.current.contains(e.target)) {
+			setOpen(false);
+		}
+	};
+
 	useEffect(() => {
 		const loadMedications = async () => {
 			const response = await axios.get('/medications.json?medication_search=true');
-
 			setMedications(response.data);
 		};
 		loadMedications();
+		document.addEventListener('click', handleOuterClick);
+
+		return () => {
+			document.removeEventListener('click', handleOuterClick);
+		};
 	}, []);
+
+	const onClickHandler = (text) => {
+		setText(text);
+		setSuggestions([]);
+	};
 
 	const onChangeHandler = (text) => {
 		let matches = [];
@@ -38,13 +55,13 @@ const MedicationAutocomplete = () => {
 				value={text}
 				onChange={(e) => onChangeHandler(e.target.value)}
 			/>
-			<div className={`drop-down-list ${open ? '' : 'hidden'}`}>
+			<div className={`drop-down-list ${open ? '' : 'hidden'}`} ref={inputRef}>
 				{suggestions.length === 0 ? (
 					<div className="drop-down-element"> No Medication Found</div>
 				) : (
 					suggestions &&
 					suggestions.map((suggestion, i) => (
-						<div className="drop-down-element" key={i} onClick={() => `${suggestion.name}`}>
+						<div className="drop-down-element" key={i} onClick={() => onClickHandler(`${suggestion.name}`)}>
 							{`${suggestion.name}`}
 						</div>
 					))
